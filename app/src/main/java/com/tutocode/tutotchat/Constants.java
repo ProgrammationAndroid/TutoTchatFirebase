ba036c34551b1bc49ae99d7f72a8d101cfb1dc31
tutocode <android.programmation@laposte.net>
2016-12-19 15:37:34 +0100

    Creation des layouts

---
+++ app/src/main/java/com/tutocode/tutotchat/Constants.java
@@ -0,0 +1,11 @@
+package com.tutocode.tutotchat;
+
+public class Constants {
+
+    public static final String USERS_DB = "users";
+    public static final String USERNAMES_DB = "usernames";
+
+    private Constants(){}
+
+
+}
